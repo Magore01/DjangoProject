cbf67c082bb8f2f06c8dc5b6915d0e2ab0bb96f1
Magore01 <r201426h@cuz.ac.zw>
2025-03-20 22:41:33 +0200

    added some new packages

---
+++ myapp/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for DjangoProject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+# from django.contrib import admin
+from django.contrib import admin
+from django.urls import path
+from . views import task_list, add_task, delete_task
+
+
+
+
+urlpatterns = [
+    path('', task_list, name='task_list'),
+    path('add/', add_task, name='add_task'),
+    path('delete/<int:task_id>/', delete_task, name='delete_task'),
+]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),
+]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    render(request,"myapp/index.html",{})
+    from .models import Task
+    from django.http import HttpResponse
+
+    def task_list(request):
+        tasks = Task.objects.all()
+        return render(request, 'todo/task_list.html', {'tasks': tasks})
+
+    def add_task(request):
+        if request.method == 'POST':
+            title = request.POST.get('title')
+            if title:
+                Task.objects.create(title=title)
+            return redirect('task_list')
+        return render(request, 'todo/add_task.html')
+
+    def delete_task(request, task_id):
+        task = Task.objects.get(id=task_id)
+        task.delete()
+        return redirect('task_list')
